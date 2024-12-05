17e918b3a3513c4a9d1e803b269d07132f4f250e
Si Thu Aung <sithuaung@nearme.com.mm>
2024-12-05 23:16:34 +0700

    DB config

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;dbname=donation',
-    'username' => '',
-    'password' => '',
+    'username' => 'postgres',
+    'password' => 'postgres',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
