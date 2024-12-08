86a8b961d0c8eb3b1f794613008be16a31b57999
Si Thu Aung <sithuaung@nearme.com.mm>
2024-12-08 10:51:25 +0700

    Fixes

--- controllers/DonarRecordController.php
+++ controllers/DonarRecordController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\DonarRecord;
+use app\models\ExpensesRecord;
 use Yii;
 use yii\web\Controller;
 
@@ -109,11 +110,13 @@ class DonarRecordController extends BaseApiController
     public function actionMonthlyStats()
     {
         $year = Yii::$app->request->get('year', date('Y'));
-        $stats = DonarRecord::find()
+        
+        // Get expenses
+        $expenses = ExpensesRecord::find()
             ->select([
                 'EXTRACT(YEAR FROM date) as year',
                 'EXTRACT(MONTH FROM date) as month',
-                'SUM(amount) as total'
+                'SUM(amount) as total_expense'
             ])
             ->groupBy(['EXTRACT(YEAR FROM date)', 'EXTRACT(MONTH FROM date)'])
             ->where(['EXTRACT(YEAR FROM date)' => $year])
@@ -124,9 +127,54 @@ class DonarRecordController extends BaseApiController
             ->asArray()
             ->all();
 
+        // Get donations
+        $donations = DonarRecord::find()
+            ->select([
+                'EXTRACT(YEAR FROM date) as year',
+                'EXTRACT(MONTH FROM date) as month',
+                'SUM(amount) as total_donation'
+            ])
+            ->groupBy(['EXTRACT(YEAR FROM date)', 'EXTRACT(MONTH FROM date)'])
+            ->where(['EXTRACT(YEAR FROM date)' => $year])
+            ->orderBy([
+                'EXTRACT(YEAR FROM date)' => SORT_DESC,
+                'EXTRACT(MONTH FROM date)' => SORT_DESC
+            ])
+            ->asArray()
+            ->all();
+
+        // Combine the results
+        $monthlyStats = [];
+        foreach ($expenses as $expense) {
+            $key = $expense['year'] . '-' . $expense['month'];
+            $monthlyStats[$key] = [
+                'year' => $expense['year'],
+                'month' => $expense['month'],
+                'total_expense' => (float)$expense['total_expense'],
+                'total_donation' => 0
+            ];
+        }
+
+        foreach ($donations as $donation) {
+            $key = $donation['year'] . '-' . $donation['month'];
+            if (!isset($monthlyStats[$key])) {
+                $monthlyStats[$key] = [
+                    'year' => $donation['year'],
+                    'month' => $donation['month'],
+                    'total_expense' => 0,
+                    'total_donation' => (float)$donation['total_donation']
+                ];
+            } else {
+                $monthlyStats[$key]['total_donation'] = (float)$donation['total_donation'];
+            }
+        }
+
+        // Sort by year and month descending
+        krsort($monthlyStats);
+
         return $this->asJson([
             'status' => 'ok',
-            'data' => $stats
+            'data' => array_values($monthlyStats)
         ]);
     }
 
@@ -135,26 +183,117 @@ class DonarRecordController extends BaseApiController
         $startYear = Yii::$app->request->get('startYear');
         $endYear = Yii::$app->request->get('endYear', date('Y'));
         
-        $query = DonarRecord::find()
+        // Get expenses
+        $expenseQuery = ExpensesRecord::find()
             ->select([
                 'EXTRACT(YEAR FROM date) as year',
-                'SUM(amount) as total'
+                'SUM(amount) as total_expense'
             ])
             ->groupBy(['EXTRACT(YEAR FROM date)'])
             ->orderBy(['EXTRACT(YEAR FROM date)' => SORT_DESC]);
             
+        // Get donations
+        $donationQuery = DonarRecord::find()
+            ->select([
+                'EXTRACT(YEAR FROM date) as year',
+                'SUM(amount) as total_donation'
+            ])
+            ->groupBy(['EXTRACT(YEAR FROM date)'])
+            ->orderBy(['EXTRACT(YEAR FROM date)' => SORT_DESC]);
+        
+        // Apply year filters to both queries
         if ($startYear) {
-            $query->andWhere(['>=', 'EXTRACT(YEAR FROM date)', $startYear]);
+            $expenseQuery->andWhere(['>=', 'EXTRACT(YEAR FROM date)', $startYear]);
+            $donationQuery->andWhere(['>=', 'EXTRACT(YEAR FROM date)', $startYear]);
         }
         if ($endYear) {
-            $query->andWhere(['<=', 'EXTRACT(YEAR FROM date)', $endYear]);
+            $expenseQuery->andWhere(['<=', 'EXTRACT(YEAR FROM date)', $endYear]);
+            $donationQuery->andWhere(['<=', 'EXTRACT(YEAR FROM date)', $endYear]);
         }
 
-        $stats = $query->asArray()->all();
+        $expenses = $expenseQuery->asArray()->all();
+        $donations = $donationQuery->asArray()->all();
+
+        // Combine the results
+        $yearlyStats = [];
+        foreach ($expenses as $expense) {
+            $yearlyStats[$expense['year']] = [
+                'year' => $expense['year'],
+                'total_expense' => (float)$expense['total_expense'],
+                'total_donation' => 0
+            ];
+        }
+
+        foreach ($donations as $donation) {
+            if (!isset($yearlyStats[$donation['year']])) {
+                $yearlyStats[$donation['year']] = [
+                    'year' => $donation['year'],
+                    'total_expense' => 0,
+                    'total_donation' => (float)$donation['total_donation']
+                ];
+            } else {
+                $yearlyStats[$donation['year']]['total_donation'] = (float)$donation['total_donation'];
+            }
+        }
+
+        // Sort by year descending
+        krsort($yearlyStats);
 
         return $this->asJson([
             'status' => 'ok',
-            'data' => $stats
+            'data' => array_values($yearlyStats)
         ]);
     }
+
+    // public function actionMonthlyStats()
+    // {
+    //     $year = Yii::$app->request->get('year', date('Y'));
+    //     $stats = DonarRecord::find()
+    //         ->select([
+    //             'EXTRACT(YEAR FROM date) as year',
+    //             'EXTRACT(MONTH FROM date) as month',
+    //             'SUM(amount) as total'
+    //         ])
+    //         ->groupBy(['EXTRACT(YEAR FROM date)', 'EXTRACT(MONTH FROM date)'])
+    //         ->where(['EXTRACT(YEAR FROM date)' => $year])
+    //         ->orderBy([
+    //             'EXTRACT(YEAR FROM date)' => SORT_DESC,
+    //             'EXTRACT(MONTH FROM date)' => SORT_DESC
+    //         ])
+    //         ->asArray()
+    //         ->all();
+
+    //     return $this->asJson([
+    //         'status' => 'ok',
+    //         'data' => $stats
+    //     ]);
+    // }
+
+    // public function actionYearlyStats()
+    // {
+    //     $startYear = Yii::$app->request->get('startYear');
+    //     $endYear = Yii::$app->request->get('endYear', date('Y'));
+        
+    //     $query = DonarRecord::find()
+    //         ->select([
+    //             'EXTRACT(YEAR FROM date) as year',
+    //             'SUM(amount) as total'
+    //         ])
+    //         ->groupBy(['EXTRACT(YEAR FROM date)'])
+    //         ->orderBy(['EXTRACT(YEAR FROM date)' => SORT_DESC]);
+            
+    //     if ($startYear) {
+    //         $query->andWhere(['>=', 'EXTRACT(YEAR FROM date)', $startYear]);
+    //     }
+    //     if ($endYear) {
+    //         $query->andWhere(['<=', 'EXTRACT(YEAR FROM date)', $endYear]);
+    //     }
+
+    //     $stats = $query->asArray()->all();
+
+    //     return $this->asJson([
+    //         'status' => 'ok',
+    //         'data' => $stats
+    //     ]);
+    // }
 }
