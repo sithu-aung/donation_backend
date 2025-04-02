c436ff54cceb1a145ac16d9ade72063b1dd9a1b4
sithu-aung <si.thuaung@innorithm.co>
2025-04-02 09:25:02 +0700

    PG

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=donation',
-    'username' => '',
+    'username' => 'postgres',
     'password' => '',
     'charset' => 'utf8',
     'enableSchemaCache' => true,
